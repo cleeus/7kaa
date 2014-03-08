cleeus/7kaa

commit 71cea8f271527351cbd03550a9b62b35abc46230

add: remove unneeded clear call

src/video/sdl/vga_sdl.cpp
@@ -563,7 +563,7 @@ void VgaSDL::flip()
       ticks = cur_ticks;
       SDL_BlitSurface(src, NULL, target, NULL);
       SDL_UpdateTexture(texture, NULL, target->pixels, window_pitch);
-      SDL_RenderClear(renderer);
+      //SDL_RenderClear(renderer);
       SDL_RenderCopy(renderer, texture, NULL, NULL);
       SDL_RenderPresent(renderer);
    }

commit b064d045827276a720ef91da01507c4d35b9acaa

VgaSDL: fix some graphics glitches caused by 71cea8f271527351cbd03550a9b62b35abc46230

src/video/sdl/vga_sdl.cpp
@@ -467,6 +467,7 @@ void VgaSDL::handle_messages()
 
 			 // update ctrl/shift/alt key state
 			 mouse.update_skey_state();
+			 SDL_RenderClear(renderer);
 			 SDL_ShowCursor(SDL_DISABLE);
 			 break;
 